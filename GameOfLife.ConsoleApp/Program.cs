f5519c537959f81de136b35249fa655cf0dc736d
sabine.petrovska <sabine.petrovska@LVAGPLTP7215>
2025-08-14 12:39:46 +0300

    test - added comment

--- GameOfLife.ConsoleApp/Program.cs
+++ GameOfLife.ConsoleApp/Program.cs
@@ -94,6 +94,8 @@ namespace GameOfLife.ConsoleApp
                     Console.Write(" ");
                 }
                 Console.WriteLine();
+
+                // comment 
             }
         }
     }
