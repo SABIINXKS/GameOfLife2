8f7e0ba8f58e96942da0e49ba3f4acedf08a1e3c
sabine.petrovska <sabine.petrovska@LVAGPLTP7215>
2025-08-18 09:26:55 +0300

    delete code from GameState.cs

--- GameOfLife.ConsoleApp/GameState.cs
+++ GameOfLife.ConsoleApp/GameState.cs
@@ -1,8 +1,1 @@
-﻿[Serializable]
-public class GameState
-{
-    public int Size { get; set; }
-    public bool[,] Field { get; set; }
-    public int GenerationCount { get; set; }
-}
-// data contains the state of the game
+﻿
