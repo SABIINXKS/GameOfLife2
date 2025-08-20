8f64fb6a227d759f50d4408ac77358cbfa4040e2
sabine.petrovska <sabine.petrovska@LVAGPLTP7215>
2025-08-20 18:14:46 +0300

    define named constants for neighbor values

--- LifeEngineLib/LifeEngineConstants.cs
+++ LifeEngineLib/LifeEngineConstants.cs
@@ -1,1 +1,9 @@
-﻿
+﻿namespace LifeEngineLib
+{
+    public static class LifeEngineConstants
+    {
+        public const int SurvivalMinNeighbors = 2;
+        public const int SurvivalMaxNeighbors = 3;
+        public const int BirthNeighbors = 3;
+    }
+}
