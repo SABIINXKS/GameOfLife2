5181071cf0c1b80bf6be055205f5062a696c7139
sabine.petrovska <sabine.petrovska@LVAGPLTP7215>
2025-08-28 12:21:35 +0300

    Refactor Program.cs to allow selection of exact game patterns for display
    - Added example patterns (Glider, Blinker) as initial fields
    - Updated ShowSelectedGames call to use user-defined patterns

--- GameOfLife.ConsoleApp/Program.cs
+++ GameOfLife.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GameOfLife.ConsoleApp
 {
@@ -7,7 +8,25 @@ namespace GameOfLife.ConsoleApp
         static void Main(string[] args)
         {
             var runner = new GameRunner();
-            runner.ShowSelectedGames(8, 10);
+
+            // Create example fields (patterns)
+            var glider = new bool[10, 10];
+            glider[1, 2] = true;
+            glider[2, 3] = true;
+            glider[3, 1] = true;
+            glider[3, 2] = true;
+            glider[3, 3] = true;
+
+            var blinker = new bool[10, 10];
+            blinker[5, 5] = true;
+            blinker[5, 6] = true;
+            blinker[5, 7] = true;
+
+            // Add patterns to the list of initial fields
+            var initialFields = new List<bool[,]> { glider, blinker };
+
+            // Show selected games on screen with specified patterns
+            runner.ShowSelectedGames(initialFields, 10);
         }
     }
 }
